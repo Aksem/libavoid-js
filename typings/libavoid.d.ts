e2acd1b317aed0c00345f099fe42beb61c11583c
Vladyslav Hnatiuk <aders1234@gmail.com>
2021-09-28 17:26:04 +0200

    Improve  project config and

--- typings/libavoid.d.ts
+++ typings/libavoid.d.ts
@@ -1,13 +1,16 @@
-declare module 'libavoid-js';
+import { AvoidLib } from "../src/index";
 
+declare module "libavoid-js";
+
+export { AvoidLib };
 declare class Point {
   constructor(x: number, y: number);
   x: number;
   y: number;
 }
 
-declare class Router {
-  constructor(flags: number);
+declare interface Router {
+  new (flags: number): Router;
 
   processTransaction(): void;
   printInfo(): void;
@@ -23,17 +26,16 @@ declare class ConnEnd {
   constructor(point: Point);
 }
 
-declare class ConnRef {
-  constructor(router: Router, srcConnEnd: ConnEnd, dstConnEnd: ConnEnd);
+declare interface ConnRef {
+  new (router: Router, srcConnEnd: ConnEnd, dstConnEnd: ConnEnd): ConnRef;
 
   displayRoute(): PolyLine;
   setSourceEndpoint(srcPoint: ConnEnd): void;
   setDestEndpoint(dstPoint: ConnEnd): void;
+  setRoutingType(type: number): void;
 }
 
-declare class Polygon {
-
-}
+declare class Polygon {}
 
 declare class Rectangle extends Polygon {
   constructor(centre: Point, width: number, height: number);
@@ -43,16 +45,19 @@ declare class ShapeRef {
   constructor(router: Router, shapePoly: Polygon);
 }
 
-interface Avoid {
+export interface Avoid {
+  [x: string]: any;
   PolyLineRouting: number;
   OrthogonalRouting: number;
 
-  ConnEnd: typeof ConnEnd,
-  ConnRef: typeof ConnRef,
-  Point: typeof Point,
-  Rectangle: typeof Rectangle,
-  Router: typeof Router;
+  ConnEnd: typeof ConnEnd;
+  ConnRef: ConnRef;
+  Point: typeof Point;
+  Rectangle: typeof Rectangle;
+  Router: Router;
   ShapeRef: typeof ShapeRef;
+
+  destroy(obj: any): void;
 }
 
 interface AvoidLibInterface {
@@ -60,3 +65,5 @@ interface AvoidLibInterface {
   load(): void;
   getInstance(): Avoid;
 }
+
+export as namespace libAvoid;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path")
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: "libavoid",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  mode: "development",
+}
