c57a6946217011960059712723e99df8b564c6cf
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-03-26 21:37:16 +0100

    Try another parameters of compiler & save artifacts

--- tools/generate.py
+++ tools/generate.py
@@ -107,7 +107,7 @@ def compile(debug=False):
   if debug:
     args += ' -g4 --source-map-base http://localhost:8080/lib/ -s RUNTIME_LOGGING=1'
   else:
-    args += ' -O3'
+    args += ' -O2'
 
   execute_command(f"""
   docker run \
@@ -115,12 +115,13 @@ def compile(debug=False):
   -v {str(pwd)}:/src -w /src/ \
   -u {os.getuid()}:{os.getgid()} \
   emscripten/emsdk \
-  emcc {args} --closure 1 -s LLD_REPORT_UNDEFINED -s FILESYSTEM=0 -s MODULARIZE=1 -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0 -s EXPORT_NAME="'initAvoidModule'" -s ALLOW_TABLE_GROWTH=1 --no-entry -Iadaptagrams/cola/ -I{generated_sources_dir}/ {compiler_sources} {post_js_args} -o {dist_dir_name}/libavoid.js
+  emcc {args} --closure 1  -flto -s FILESYSTEM=0 -s MODULARIZE=1 -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0 -s EXPORT_NAME="'initAvoidModule'" -s ALLOW_TABLE_GROWTH=1 --no-entry -Iadaptagrams/cola/ -I{generated_sources_dir}/ {compiler_sources} {post_js_args} -o {dist_dir_name}/libavoid.js
   """, logger)
   # -s CLOSURE_WARNINGS=error .. will show all warnings and errors, but also of emcc
   # -g .. for debug, -g4 is max level with source maps for browser
   # EXPORT_NAME ...  explanation: https://stackoverflow.com/a/30153119
   # source-map-base is required for browser recognition of source maps
+  # -s LLD_REPORT_UNDEFINED
 
 
 def main():
